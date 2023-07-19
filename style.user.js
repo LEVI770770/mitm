5ecd6821e20aa518eac2f49a961b2dd545bf683a
LEVI770770 <82899765+LEVI770770@users.noreply.github.com>
2023-07-19 19:18:51 +0300

    style.user.js

--- style.user.js
+++ style.user.js
@@ -2,7 +2,7 @@
 // @name         new style of chat in mitmachim top
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  new style of chat in mitmachim top
 // @author       levi h
 // @match        https://mitmachim.top/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mitmachim.top
